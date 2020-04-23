69577e5c30255719ac23666b1c25f7ee178f1a54
LOG1CRS <0204571@up.edu.mx>
2020-04-22 21:07:22 -0500

    To Scene class, its constructor, getters and setters and a addObject method were added

--- src/Tools/Scene.java
+++ src/Tools/Scene.java
@@ -2,9 +2,45 @@ package Tools;
 
 import Objects.Object3D;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Scene {
+    private Camera camera;
     private List<Object3D> objects;
+
+    /**
+     * Scene constructor
+     * Initialize the ArrayList of objects.
+     */
+    public Scene(){
+        setObjects(new ArrayList<Object3D>());
+    }
+
+    //Getters & Setters
+
+    public Camera getCamera() {
+        return camera;
+    }
+
+    public void setCamera(Camera camera) {
+        this.camera = camera;
+    }
+
+    public List<Object3D> getObjects() {
+        return objects;
+    }
+
+    public void setObjects(List<Object3D> objects) {
+        this.objects = objects;
+    }
+
+    /**
+     * Add object to scene
+     * @param object
+     */
+    public void addObject(Object3D object){
+        getObjects().add(object);
+    }
 }
