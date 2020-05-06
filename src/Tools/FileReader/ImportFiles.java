521fd7bd730d0c4c80bd61f623f7ba6f5c060a9c
LOG1CRS <0204571@up.edu.mx>
2020-05-06 17:21:39 -0500

    importFiles class was added

---
+++ src/Tools/FileReader/ImportFiles.java
@@ -0,0 +1,63 @@
+package Tools.FileReader;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+
+public class ImportFiles {
+    public static void importFile(){
+
+        //Change the JFileChooser window style
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+        JFileChooser selector = new JFileChooser();
+        JFrame parentFrame = new JFrame();
+
+        chooserConfiguration(selector, parentFrame);
+        System.out.println("Select all .obj files you want to add to the scene");
+
+        File[] input = askForTheFiles(selector, parentFrame);
+
+    }
+
+    /**
+     * chooserConfiguration, configure the chooser to allow only photos and videos and multi selection
+     * @param selector
+     * @param parentFrame
+     */
+    private static void chooserConfiguration(JFileChooser selector, JFrame parentFrame){
+        parentFrame.setAlwaysOnTop(true);
+        parentFrame.requestFocus();
+
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("OBJ Files", "obj");
+        selector.setFileFilter(filter);
+        selector.setMultiSelectionEnabled(true);
+    }
+
+    /**
+     *askForTheFIle, show the selector and return the Files selected
+     * @param selector
+     * @param parentFrame
+     * @return inputFile[]
+     */
+    private static File[] askForTheFiles(JFileChooser selector, JFrame parentFrame)
+    {
+        int result;
+
+        //loop that ensures the user selects a file
+        do{
+            System.out.println("Selector is open, please select the OBJ files: ");
+            result = selector.showOpenDialog(parentFrame);
+        }while(!(result == JFileChooser.APPROVE_OPTION));
+
+        //Destroy the window
+        parentFrame.dispose();
+
+        return selector.getSelectedFiles();
+    }
+}
